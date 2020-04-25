5d0f71d99dbfa4c944d0b6722512083b34ac571e
dgnzlz <54804883+dgnzlz@users.noreply.github.com>
2020-04-25 16:35:50 +0200

    Create RewardFunction.py

---
+++ RewardFunction.py
@@ -0,0 +1,22 @@
+def reward_function(params):
+    
+    # Read input parameters
+    all_wheels_on_track = params['all_wheels_on_track']
+    distance_from_center = params['distance_from_center']
+    track_width = params['track_width']
+    progress=params['progress']
+    
+    # Give a very low reward by default
+    reward = 1e-3
+
+    # Give a high reward if no wheels go off the track and
+    # the agent is somewhere in between the track borders
+    if all_wheels_on_track and (0.5*track_width - distance_from_center) >= 0.05:
+        reward = 1.0
+    
+    #Incentive for finishing the lap
+    if progress == 100:
+        reward = reward + 100
+
+    # Always return a float value
+    return float(reward)
