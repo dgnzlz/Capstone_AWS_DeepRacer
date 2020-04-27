fd199f76cbbe5a12761772fa2a27762eac017fc1
dgnzlz <54804883+dgnzlz@users.noreply.github.com>
2020-04-27 23:54:59 +0200

    Update RewardFunction.py

--- RewardFunction.py
+++ RewardFunction.py
@@ -1,6 +1,6 @@
 def reward_function(params):
     
-    # Read input parameters
+    ## Read input parameters ##
     all_wheels_on_track = params['all_wheels_on_track']
     x = params['x']
     y = params['y']
@@ -15,25 +15,29 @@ def reward_function(params):
     waypoints = params['waypoints']
     closest_waypoints = params['closest_waypoints']
     
-    # Define the default reward
+    ## Define the default reward ##
     reward = 1.0
-    # Penalize if the car goes too slow
+    
+    ## Penalize if the car goes too slow (relative penalization) ##
     if speed < 1:
         reward *= 0.5
     elif speed < 1.5:
         reward *= 0.9
-        
-    REALISTIC_NUMB_STEPS = 400
+    
+    ## Incentive for using less steps (absolute reward) ##
+    SLOWEST_STEPS = 500 # about 35 seconds
+    FASTEST_STEPS = 200 # about 13 seconds
     SPLIT_TRACK_N_PARTS = 20
-    for i in range(100, REALISTIC_NUMB_STEPS+100, 10):
+    for i in range(SLOWEST_STEPS, FASTEST_STEPS, -10):
         check_after_n_steps = int(i / SPLIT_TRACK_N_PARTS)
         if (steps % check_after_n_steps) == 0 and progress > (steps / i) * 100:
             reward += 1
     
-    #Incentive for finishing the lap
+    ## Incentive for finishing the lap (absolute reward) ##
     if progress == 100:
         reward += 100
 
+    ## Zero reward if off track ##
     if not all_wheels_on_track or (0.5*track_width - distance_from_center) < 0.05:
         reward = 1e-3
         
