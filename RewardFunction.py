ef9cd3bc123112d0faca8c5340712ca3207b3ccc
dgnzlz <54804883+dgnzlz@users.noreply.github.com>
2020-04-29 20:28:07 +0200

    Update RewardFunction.py

--- RewardFunction.py
+++ RewardFunction.py
@@ -27,26 +27,6 @@ def reward_function(params):
     elif speed < 1.5:
         reward *= 0.9
     
-    ## Penalize if heading of car is too far off next waypoint (relative penalization) ##
-    # Calculate the direction of the center line based on the closest waypoints
-    next_point = waypoints[closest_waypoints[1]]
-    prev_point = waypoints[closest_waypoints[0]]
-    # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
-    track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0]) 
-    # Convert to degree
-    track_direction = math.degrees(track_direction)
-    # Calculate the difference between the track direction and the heading direction of the car
-    direction_diff = abs(track_direction - heading)
-    if direction_diff > 180:
-        direction_diff = 360 - direction_diff
-    # Penalize the reward if the difference is too large
-    if direction_diff > 30.0:
-        reward *= 0.1
-    elif direction_diff > 20.0:
-        reward *= 0.5
-    elif direction_diff > 10.0:
-        reward *= 0.9
-    
     ## Incentive for using less steps (absolute reward) ##
     ## Reminder: This
     SLOWEST_STEPS = 500 # about 35 seconds
@@ -64,6 +44,22 @@ def reward_function(params):
         # Always give 100 reward, but more if reward from step-for-loop is higher
         reward += max(100, (SLOWEST_STEPS-steps)/10*SPLIT_TRACK_N_PARTS*finish_multiple)
 
+    ## Penalize if heading of car is too far off next waypoint (relative penalization) ##
+    # Calculate the direction of the center line based on the closest waypoints
+    next_point = waypoints[closest_waypoints[1]]
+    prev_point = waypoints[closest_waypoints[0]]
+    # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
+    track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0]) 
+    # Convert to degree
+    track_direction = math.degrees(track_direction)
+    # Calculate the difference between the track direction and the heading direction of the car
+    direction_diff = abs(track_direction - heading)
+    if direction_diff > 180:
+        direction_diff = 360 - direction_diff
+    # Penalize the reward if the difference is too large
+    if direction_diff > 45.0:
+        reward = 1e-3
+        
     ## Zero reward if off track ##
     if not all_wheels_on_track or (0.5*track_width - distance_from_center) < 0.05:
         reward = 1e-3
